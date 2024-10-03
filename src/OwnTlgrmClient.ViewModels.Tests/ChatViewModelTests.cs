328686eb08d4a2d4806b4c85021c611b0f22f46b
ib-user <>
2024-10-03 08:10:00 +0300

    Added tests for ChatViewModel

---
+++ src/OwnTlgrmClient.ViewModels.Tests/ChatViewModelTests.cs
@@ -0,0 +1,42 @@
+using FluentAssertions;
+using OwnTlgrmClient.ViewModels.Components;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+
+namespace OwnTlgrmClient.ViewModels.Tests;
+
+public class ChatViewModelTests
+{
+    [Fact]
+    public void SendMessage_ShouldAddMessage_WhenMessageInputIsNotEmpty()
+    {
+        // Arrange
+        var viewModel = new ChatViewModel
+        {
+            MessageInput = "Test message"
+        };
+
+        // Act
+        viewModel.SendMessageCommand.Execute().Subscribe();
+
+        // Assert
+        viewModel.Messages.Should().HaveCount(1);
+        viewModel.Messages[0].Content.Should().Be("Test message");
+    }
+
+    [Fact]
+    public void SendMessage_ShouldNotAddMessage_WhenMessageInputIsEmpty()
+    {
+        // Arrange
+        var viewModel = new ChatViewModel
+        {
+            MessageInput = "" // Empty input
+        };
+
+        // Act
+        viewModel.SendMessageCommand.Execute().Subscribe();
+
+        // Assert
+        viewModel.Messages.Should().HaveCount(0);
+    }
+}
